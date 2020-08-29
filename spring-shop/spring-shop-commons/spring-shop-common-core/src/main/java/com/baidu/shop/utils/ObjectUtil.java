15db9cbd9d528e4f9b626d33ff9f60eedf53261a
输入你的用户名huangyanan <nan141242d@163.com>
2020-08-29 09:37:09 +0800

    分类查询新增修改删除 api /swargger2

---
+++ spring-shop/spring-shop-commons/spring-shop-common-core/src/main/java/com/baidu/shop/utils/ObjectUtil.java
@@ -0,0 +1,15 @@
+package com.baidu.shop.utils;
+
+/**
+ * @ClassName ObjectUtil
+ * @Description: TODO
+ * @Author huangyanan
+ * @Date 2020/8/28
+ * @Version V1.0
+ **/
+public class ObjectUtil {
+    public static Boolean isNull(Object obj){
+
+        return null == obj;
+    }
+}
